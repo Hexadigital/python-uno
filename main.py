df79ac0f0c4914018ab8cba18365edcb30390d9a
Hexadigital <hexadigital@vastitune.com>
2015-11-03 18:22:43 -0500

    First release

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def GenerateDeck():
+	zeroes = [["Green", "0"], ["Blue", "0"], ["Yellow", "0"], ["Red", "0"]]
+	colors = ["Green", "Blue", "Yellow", "Red"]
+	values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Draw Two", "Reverse"]
+	wilds = [["Colorless", "Wild"], ["Colorless", "Draw Four"]]
+	deck = []
+	for color in colors:
+		for value in values:
+			deck += [[color, value]]
+	return deck
+
+
+
+
+
+
+UnoDeck = GenerateDeck()
+print UnoDeck
