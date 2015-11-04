b809c1812105acfaabee6080c2aed78bad227a4b
Hexadigital <hexadigital@vastitune.com>
2015-11-03 19:07:31 -0500

    Added the capability to generate a hand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
 @author Hexadigital
 '''
+from random import randint
 
 def GenerateDeck():
 	zeroes = [["Green", "0"], ["Blue", "0"], ["Yellow", "0"], ["Red", "0"]]
@@ -15,11 +16,22 @@ def GenerateDeck():
 	for i in range(0,4):
 		deck += wilds
 	return deck
+	
+def GenerateHand():
+	hand = []
+	# Make a hand of 7 cards
+	for i in range(0, 7):
+		# Pick a random card from the deck
+		cardnum = randint(0, len(UnoDeck) - 1)
+		print "The card number is " + str(cardnum) + ", the length of the deck is " + str(len(UnoDeck))
+		# Add the card to the hand
+		hand += [UnoDeck[cardnum]]
+		# Remove the card from the deck
+		UnoDeck.remove(UnoDeck[cardnum])
+	# Give the player their hand
+	return hand
 
-
-
-
-
-
+# Let's start the game!
 UnoDeck = GenerateDeck()
-print UnoDeck
+p1hand = GenerateHand()
+print p1hand
