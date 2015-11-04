2a465a327b1fb49d14ba167b53674158eadb1df7
Hexadigital <hexadigital@vastitune.com>
2015-11-04 00:10:20 -0500

    Added random events

--- main.py
+++ main.py
@@ -172,32 +172,51 @@ def PrettifyCards(listofcards):
 	# Return the list of colored cards
 	return returnstring
 
+def RandomEvent():
+	chance = randint(0, 100)
+	if chance == 0:
+		print("The dealer smiles.")
+	if chance == 1:
+		print("You adjust your posture.")
+	if chance == 2:
+		print("You feel a " + Style.BRIGHT + colored("chill", 'cyan') + Style.RESET_ALL + " run up your back.")
+	if chance == 3:
+		print("The lady glances around nervously.")
+	if chance == 4:
+		print("The bartender looks away once you notice him staring at you.")
+	if chance == 5:
+		print("The man's cigarette isn't " + Style.BRIGHT + colored("burning", lightercolor[randint(0,1)]) + Style.RESET_ALL + " down.")
+	if chance == 6:
+		print("You hear someone call your name, but you must have been mistaken.")
+		print("Nobody here knows your name.")
+
 # Load the config
 execfile('config.txt')	
 
 # Let's start the game!
 init()
 lightercolor = ["yellow", "red"]
+# Generate the deck
+UnoDeck = GenerateDeck()
+# Generate the players' hands
+p1hand = GenerateHand()
+p2hand = GenerateHand()
+p3hand = GenerateHand()
+p4hand = GenerateHand()
+
 # A short intro
 '''print("Welcome to the world of Uno!")
 print("You sit down at a table with a few other people.")
-sleep(3)'''
-# Generate the deck
-UnoDeck = GenerateDeck()
-'''print("The dealer begins to shuffle the deck.")
+sleep(3)
+print("The dealer begins to shuffle the deck.")
 sleep(1)
 print("\nWhile you are waiting, you examine the other players.")
 sleep(5)
 print("Two of them seem to be composed, but the other is a nervous wreck.")
 sleep(3)
 print("You watch as he " + Style.BRIGHT + colored("lights", lightercolor[randint(0,1)]) + Style.RESET_ALL + " a cigarette to calm his nerves.")
-sleep(3)'''
-# Generate the players' hands
-p1hand = GenerateHand()
-p2hand = GenerateHand()
-p3hand = GenerateHand()
-p4hand = GenerateHand()
-'''print("\nThe dealer gives everyone their hands.")
+sleep(3)
+print("\nThe dealer gives everyone their hands.")
 sleep(1)
 print("You pick your cards up, but don't look at them just yet.")
 sleep(2)'''
@@ -212,3 +231,4 @@ while len(p1hand) != 0 and len(p2hand) != 0 and len(p3hand) != 0 and len(p4hand)
 	tablecard = P2Turn()
 	tablecard = P3Turn()
 	tablecard = P4Turn()
+	RandomEvent()
