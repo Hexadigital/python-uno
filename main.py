425e4113f3cc4c8644aa82e190c94af2a5579505
Hexadigital <hexadigital@vastitune.com>
2015-11-03 19:41:12 -0500

    Made it so that you can print out coloured hands

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 @author Hexadigital
 '''
 from random import randint
+from colorama import init, Fore, Style
+from termcolor import colored
 
 def GenerateDeck():
 	zeroes = [["Green", "0"], ["Blue", "0"], ["Yellow", "0"], ["Red", "0"]]
@@ -24,15 +26,26 @@ def GenerateHand():
 	for i in range(0, 7):
 		# Pick a random card from the deck
 		cardnum = randint(0, len(UnoDeck) - 1)
-		print "The card number is " + str(cardnum) + ", the length of the deck is " + str(len(UnoDeck))
 		# Add the card to the hand
 		hand += [UnoDeck[cardnum]]
 		# Remove the card from the deck
 		UnoDeck.remove(UnoDeck[cardnum])
 	# Give the player their hand
 	return hand
-
+	
+def PrintPretty(listofcards):
+	for i in range(0, len(listofcards) - 1):
+		if listofcards[i][0] == "Blue":
+			print(Style.BRIGHT + colored(listofcards[i][0] + " " + listofcards[i][1], 'cyan') + Style.RESET_ALL)
+		elif listofcards[i][0] != "Colorless":
+			print(Style.BRIGHT + colored(listofcards[i][0] + " " + listofcards[i][1], listofcards[i][0].lower()) + Style.RESET_ALL)
+		else:
+			print(Style.BRIGHT + colored(listofcards[i][0] + " " + listofcards[i][1], 'white') + Style.RESET_ALL)
+			
 # Let's start the game!
+init()
 UnoDeck = GenerateDeck()
 p1hand = GenerateHand()
-print p1hand
+PrintPretty(p1hand)
+#print("Welcome to the world of Uno!")
+#numplayers = int(input("How many players will be playing today? (0-10): "))
